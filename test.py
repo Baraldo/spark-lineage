2a28b3e5d43e6cd7956f3950697c34770ac99216
Baraldo <luiz.felipe.m.baraldo@hotmail.com>
2020-06-18 00:51:52 -0300

    lineage class

--- test.py
+++ test.py
@@ -5,14 +5,18 @@ from pyspark.sql.functions import expr
 import pyspark.sql.functions as f
 from spark_lineage.spark_lineage import produce, extract
 spark = (SparkSession.builder.appName("test").getOrCreate())
+
+
 @produce
 def return_df(df):
-    return df.withColumn(colName = 'df', col = f.lit('kappa'))
+    return df.withColumn(colName = 'df', col = f.col('age'))
+
 @extract
 def extract_df(alias):
     return spark.createDataFrame(["10","11","13"], "string").toDF("age")
+
 df = extract_df(alias='kippo')
-df = spark.createDataFrame(["10","11","13"], "string").toDF("age")
+
 df = return_df(df)
-#df = return_df(df)
-df.show()
+
+print(df.lineage)
